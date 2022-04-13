f4d7df600a4873c97e3066708fd092e09e751b00
Jake Dance <jake.dance1991@gmail.com>
2022-04-13 16:21:34 +0100

    added weather function via API

--- thermostat/interface.js
+++ thermostat/interface.js
@@ -1,10 +1,30 @@
 
 
 document.addEventListener("DOMContentLoaded", () => {
+  const displayWeather = (city) => {
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f47a2e709e9dcfc13b376d1c7198d5d5&units=metric`
+
+  fetch(url)
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => {
+      document.querySelector('#current-temperature').innerText = data.main.temp;
+    })
+  }
   const updateTemperature = () => {
     document.querySelector('#temperature').innerText = thermostat.temperature();
     document.querySelector('#temperature').className = thermostat.usage();
   }
+
+  displayWeather('London');
+
+  document.querySelector('#select-city').addEventListener('submit', (event) => {
+  event.preventDefault();
+  const city = document.querySelector('#current-city').value;
+
+  displayWeather(city);
+  })
   
   const thermostat = new Thermostat();
   updateTemperature();
